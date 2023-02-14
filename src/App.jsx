63e680f11f9e774407772036623c322b49776a03
Sushil1204 <sushilspundkar@gmail.com>
2023-02-14 21:47:22 +0530

    scroll to top button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Blogs from "./components/Blogs";
 import Contact from "./components/Contact";
-import { ThemeProvider } from "./Context/ThemeContext.jsx";
+import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+      /* you can also use 'auto' behaviour
+         in place of 'smooth' */
+    });
+  };
+
+  window.addEventListener("scroll", toggleVisible);
 
   return (
     <ThemeProvider>
@@ -16,6 +37,12 @@ function App() {
         <Projects/>
         <Blogs/>
         <Contact/>
+      {visible && (<button
+        class="bg-indigo-600 text-white font-bold py-2 px-4 rounded-full float-right -mt-14 outline-none animate-bounce "
+        onClick={scrollToTop}
+      >
+        <p className="text-xl">Go Up</p>
+      </button>)}
       </div>
     </ThemeProvider>
   );
